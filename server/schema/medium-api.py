d8281f9cad9451487e5ce56ca67cd3eaf2373eb0
Ryan Collins <rccproductions@mac.com>
2016-09-12 22:45:09 -0400

    Feat: setup graphql schema

--- server/schema/medium-api.py
+++ server/schema/medium-api.py
@@ -1,4 +1,5 @@
-import requests, json
+import requests
+import json
 import os.path
 
 def print_json(jsonData):
@@ -10,19 +11,20 @@ def get_posts_from_url(url, params):
 	r = requests.get(url, params=params)
 	index = r.text.index(';') + 5
 	data = json.loads(r.text[index:])
-	payload = data["payload"]
-	posts = payload["posts"]
+	posts = data["payload"]["posts"]
 	return parse_posts_into_dict(posts)
 
 
 def parse_posts_into_dict(posts):
 	posts_list = []
-	for i, post in enumerate(posts):
+	for post in posts:
 		posts_dict = {}
+		snippet = post["virtuals"]["snippet"]
+		emailSnippet = post["virtuals"]["emailSnippet"]
 		posts_dict["id"] = post["id"]
 		posts_dict["title"] = post["title"]
 		posts_dict["uniqueSlug"] = post["uniqueSlug"]
-		posts_dict["snippet"] = post["virtuals"]["snippet"] + post["virtuals"]["emailSnippet"]
+		posts_dict["snippet"] = snippet + emailSnippet
 		posts_dict["recommendations"] = post["virtuals"]["usersBySocialRecommends"]
 		posts_dict["image"] = post["virtuals"]["previewImage"]["imageId"]
 		posts_dict["tags"] = post["virtuals"]["tags"]
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,16 +1,15 @@
 import {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLInt,
   GraphQLString,
   GraphQLList,
 } from '../../node_modules/graphql';
 
 import pythonShell from 'python-shell';
 import fs from 'fs';
+const store = {};
 
-let posts = {};
-const loadPosts = () =>
+const runPythonScript = () =>
   new Promise((resolve, reject) => {
     const options = {
       scriptPath: '/Users/myMac/Developer/works-in-progress/react-weekly/server/schema',
@@ -21,11 +20,76 @@ const loadPosts = () =>
     });
   });
 
-loadPosts().then(() => {
-  fs.readFileSync('./posts.json', 'utf-8', (err, contents) => {
-    if (err) {
-      console.log(err);
-    }
-    posts = contents;
+const loadPosts = () => new Promise((resolve, reject) => {
+  runPythonScript().then(() => {
+    fs.readFileSync('./posts.json', 'utf-8', (err, contents) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(contents);
+    });
   });
+});
+
+const parseTag = (tag) => ({
+  name: tag.name,
+  slug: tag.slug,
+});
+
+const parseTags = (tags) => {
+  const newTags = [];
+  for (const tag of tags) {
+    newTags.push(parseTag(tag));
+  }
+  return newTags;
+};
+
+const TagType = new GraphQLObjectType({
+  name: 'Tag',
+  fields: () => ({
+    slug: { type: GraphQLString },
+    name: { type: GraphQLString },
+  }),
+});
+
+const RecommendationType = new GraphQLObjectType({
+  name: 'Recommendation',
+  fields: () => ({
+    name: { type: GraphQLString },
+  }),
+});
+
+const PostType = new GraphQLObjectType({
+  name: 'Post',
+  fields: () => ({
+    title: { type: GraphQLString },
+    snippet: { type: GraphQLString },
+    recommendations: { type: new GraphQLList(RecommendationType) },
+    tags: { type: new GraphQLList(TagType) },
+    uniqueSlug: { type: GraphQLString },
+  }),
+});
+
+const StoreType = new GraphQLObjectType({
+  name: 'Store',
+  fields: () => ({
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve: () => loadPosts().then(posts => posts),
+    },
+  }),
+});
+
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    store: {
+      type: StoreType,
+      resolve: () => store,
+    },
+  }),
+});
+
+export default new GraphQLSchema({
+  query: QueryType,
 });
