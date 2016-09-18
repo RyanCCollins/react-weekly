b9f3d4cdc61da951540493afcca2a9d83465a554
Ryan Collins <rccproductions@mac.com>
2016-09-18 16:17:41 -0400

    Feat: add baseURL to apolloclient network interface

--- app/src/apolloClient.js
+++ app/src/apolloClient.js
@@ -1,11 +1,12 @@
-
 import ApolloClient, {
   createNetworkInterface,
   addTypeName,
 } from 'apollo-client';
+const baseUrl = process.env.BASE_URL || 'http://0.0.0.0:1339/';
+const url = `${baseUrl}api`;
 
 const client = new ApolloClient({
-  networkInterface: createNetworkInterface('http://0.0.0.0:1337/api'),
+  networkInterface: createNetworkInterface(url),
   queryTransformer: addTypeName,
 });
 
