f10a01464ed34155ea85c035da327c167d919adb
Ryan Collins <rccproductions@mac.com>
2016-09-24 17:12:18 -0400

    Feat: integrate component test for the static element

--- app/src/components/Static/tests/index.test.js
+++ app/src/components/Static/tests/index.test.js
@@ -1,11 +1,27 @@
 import Static from '../index';
-
 import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
+import Markdown from 'grommet/components/Markdown';
+import Section from 'grommet/components/Section';
+import Article from 'grommet/components/Article';
 
 describe('<Static />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('should have expected components', () => {
+    const wrapper = shallow(
+      <Static
+        content="Hello world"
+        hasBoxShadow
+      />
+    );
+    expect(
+      wrapper.find(<Markdown />)
+    ).toExist();
+    expect(
+      wrapper.find(<Section />)
+    ).toExist();
+    expect(
+      wrapper.find(<Article />)
+    ).toExist();
   });
 });
